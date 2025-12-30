7503d53dcfb46ba47f79098f25d539c5b2c4444b
kishii4726 <keita.ishii@outlook.jp>
2025-12-30 14:56:42 +0900

    rdsのチェック項目追加

--- cmd/rds.go
+++ cmd/rds.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"context"
 	"log"
+	"strings"
 
+	"awsselfrev/internal/aws/api"
 	"awsselfrev/internal/color"
 	"awsselfrev/internal/config"
 	"awsselfrev/internal/table"
@@ -20,12 +22,16 @@ var rdsCmd = &cobra.Command{
 	Long: `This command checks various RDS configurations and best practices such as:
 - Storage encryption
 - Deletion protection
-- Log exports`,
+- Log exports
+- Backup enabled
+- Default parameter group usage
+- Public accessibility`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg := config.LoadConfig()
 		rules := config.LoadRules()
 		tbl := table.SetTable()
 		client := rds.NewFromConfig(cfg)
+		_, _, _ = color.SetLevelColor()
 
 		checkRDSConfigurations(client, tbl, rules)
 
@@ -33,7 +39,7 @@ var rdsCmd = &cobra.Command{
 	},
 }
 
-func checkRDSConfigurations(client *rds.Client, table *tablewriter.Table, rules config.RulesConfig) {
+func checkRDSConfigurations(client api.RDSClient, table *tablewriter.Table, rules config.RulesConfig) {
 	resp, err := client.DescribeDBClusters(context.TODO(), &rds.DescribeDBClustersInput{})
 	if err != nil {
 		log.Fatalf("Failed to describe DB clusters: %v", err)
@@ -43,6 +49,8 @@ func checkRDSConfigurations(client *rds.Client, table *tablewriter.Table, rules 
 		checkStorageEncryption(cluster, table, rules)
 		checkDeletionProtection(cluster, table, rules)
 		checkLogExports(cluster, table, rules)
+		checkClusterBackupEnabled(cluster, table, rules)
+		checkClusterDefaultParameterGroup(cluster, table, rules)
 		checkDBInstances(client, cluster.DBClusterMembers, table, rules)
 	}
 }
@@ -61,7 +69,6 @@ func checkDeletionProtection(cluster types.DBCluster, table *tablewriter.Table, 
 	}
 }
 
-// TODO:ログ種別ごとに確認できるようにする
 func checkLogExports(cluster types.DBCluster, table *tablewriter.Table, rules config.RulesConfig) {
 	if len(cluster.EnabledCloudwatchLogsExports) == 0 {
 		rule := rules.Get("rds-log-export")
@@ -69,7 +76,21 @@ func checkLogExports(cluster types.DBCluster, table *tablewriter.Table, rules co
 	}
 }
 
-func checkDBInstances(client *rds.Client, members []types.DBClusterMember, table *tablewriter.Table, rules config.RulesConfig) {
+func checkClusterBackupEnabled(cluster types.DBCluster, table *tablewriter.Table, rules config.RulesConfig) {
+	if cluster.BackupRetentionPeriod != nil && *cluster.BackupRetentionPeriod == 0 {
+		rule := rules.Get("rds-backup-enabled")
+		table.Append([]string{rule.Service, color.ColorizeLevel(rule.Level), *cluster.DBClusterIdentifier, rule.Issue})
+	}
+}
+
+func checkClusterDefaultParameterGroup(cluster types.DBCluster, table *tablewriter.Table, rules config.RulesConfig) {
+	if cluster.DBClusterParameterGroup != nil && strings.HasPrefix(*cluster.DBClusterParameterGroup, "default.") {
+		rule := rules.Get("rds-default-parameter-group")
+		table.Append([]string{rule.Service, color.ColorizeLevel(rule.Level), *cluster.DBClusterIdentifier, rule.Issue})
+	}
+}
+
+func checkDBInstances(client api.RDSClient, members []types.DBClusterMember, table *tablewriter.Table, rules config.RulesConfig) {
 	for _, member := range members {
 		resp, err := client.DescribeDBInstances(context.TODO(), &rds.DescribeDBInstancesInput{
 			DBInstanceIdentifier: member.DBInstanceIdentifier,
@@ -80,6 +101,8 @@ func checkDBInstances(client *rds.Client, members []types.DBClusterMember, table
 
 		for _, instance := range resp.DBInstances {
 			checkAutoMinorVersionUpgrade(instance, table, rules)
+			checkInstanceDefaultParameterGroup(instance, table, rules)
+			checkPublicAccessibility(instance, table, rules)
 		}
 	}
 }
@@ -87,6 +110,23 @@ func checkDBInstances(client *rds.Client, members []types.DBClusterMember, table
 func checkAutoMinorVersionUpgrade(instance types.DBInstance, table *tablewriter.Table, rules config.RulesConfig) {
 	if instance.AutoMinorVersionUpgrade != nil && *instance.AutoMinorVersionUpgrade {
 		rule := rules.Get("rds-auto-minor-version-upgrade")
+		table.Append([]string{rule.Service, color.ColorizeLevel(rule.Level), *instance.DBInstanceIdentifier, rule.Issue})
+	}
+}
+
+func checkInstanceDefaultParameterGroup(instance types.DBInstance, table *tablewriter.Table, rules config.RulesConfig) {
+	for _, pg := range instance.DBParameterGroups {
+		if pg.DBParameterGroupName != nil && strings.HasPrefix(*pg.DBParameterGroupName, "default.") {
+			rule := rules.Get("rds-default-parameter-group")
+			table.Append([]string{rule.Service, color.ColorizeLevel(rule.Level), *instance.DBInstanceIdentifier, rule.Issue})
+			break // Report once per instance
+		}
+	}
+}
+
+func checkPublicAccessibility(instance types.DBInstance, table *tablewriter.Table, rules config.RulesConfig) {
+	if instance.PubliclyAccessible != nil && *instance.PubliclyAccessible {
+		rule := rules.Get("rds-public-access")
 		table.Append([]string{rule.Service, color.ColorizeLevel(rule.Level), *instance.DBInstanceIdentifier, rule.Issue})
 	}
 }
